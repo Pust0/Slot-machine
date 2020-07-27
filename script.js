dbd882e596e1f0d9b69ecebf7807cfe00e422746
Varun <varunbhouria78@gmail.com>
2020-07-28 05:10:32 +0530

    basic slot machine with speed changer

---
+++ script.js
@@ -0,0 +1,144 @@
+let value1 = document.getElementById("value1");
+let value2 = document.getElementById("value2");
+let value3 = document.getElementById("value3");
+
+let inpSpeed = document.getElementById("inpSpeed");
+
+let values = [
+  "😀",
+  "😃",
+  "😄",
+  "😁",
+  "😆 ",
+  "😅",
+  "😂",
+  "🤣",
+  "😊",
+  "😇",
+  "🙂",
+  "🙃",
+  "😉",
+  "😌",
+  "😍",
+  "🥰",
+  "😘",
+  "😗",
+  "😙",
+  "😚",
+  "😋",
+  "😛",
+  "😝",
+  "😜",
+  "🤪",
+  "🤨",
+  "🧐",
+  "🤓",
+  "😎",
+  "🤩",
+  "🥳",
+  "😏",
+  "😒",
+  "😞",
+  "😔",
+  "😟",
+  "😕",
+  "🙁",
+  "☹️",
+  "😣",
+  "😖",
+  "😫",
+  "😩",
+  "🥺",
+  "😢",
+  "😭",
+  "😤",
+  "😠",
+  "😡",
+  "🤬",
+  "🤯",
+  "😳",
+  "🥵",
+  "🥶",
+  "😱",
+  "😨",
+  "😰",
+  "😥",
+  "😓",
+  "🤗",
+  "🤔",
+  "🤭",
+  "🤫",
+  "🤥",
+  "😶",
+  "😐",
+  "😑",
+  "😬",
+  "🙄",
+  "😯",
+  "😦",
+  "😧",
+  "😮",
+  "😲",
+  "🥱",
+  "😴",
+  "🤤",
+  "😪",
+  "😵",
+  "🤐",
+  "🥴",
+  "🤢",
+  "🤮",
+  "🤧",
+  "😷",
+  "🤒",
+  "🤕",
+  "🤑",
+  "🤠",
+  "😈",
+  "👿",
+  "👹",
+  "👺",
+  "🤡",
+  "💩",
+  "👻",
+  "💀",
+  "☠️",
+  "👽",
+  "👾",
+  "🤖",
+  "🎃",
+  "😺",
+  "😸",
+  "😹",
+  "😻",
+  "😼",
+  "😽",
+  "🙀",
+  "😿",
+  "😾",
+];
+
+function getRandomValue() {
+  return values[Math.floor(Math.random() * 111)];
+}
+
+let animationId;
+function updateAnimation(newSpeed) {
+  if (animationId) clearInterval(animationId);
+
+  animationId = setInterval(() => {
+    value1.innerText = getRandomValue();
+    value2.innerText = getRandomValue();
+    value3.innerText = getRandomValue();
+  }, 1000 / newSpeed);
+}
+
+inpSpeed.onchange = function (ev) {
+  console.log("value changed");
+
+  //document.documentElement is the ":root" of css
+
+  document.documentElement.style.setProperty("--speed", ev.target.value);
+
+  updateAnimation(ev.target.value);
+};
